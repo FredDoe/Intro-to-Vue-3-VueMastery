6d1e9286d599fb864212ee0794c317ef96ab4ace
Godfred Doe <godfredoe9@gmail.com>
2024-02-16 04:47:56 +0000

    vue app created in lesson 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product: "Socks",
+            description: "A comfortable feet soothing socks made from high quality materials."
+        }
+    }
+})
